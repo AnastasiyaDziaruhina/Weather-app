f5cfda0a6db2f6d4520deb2ffd8da38fab06f605
stacysfish <st.vitalievna@yandex.ru>
2022-07-18 19:40:20 +0200

    Initial commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Stacy on 14.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
